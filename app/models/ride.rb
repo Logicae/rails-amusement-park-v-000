fdfde0623a3ab91a1e6a4e748c3a4d05e716fb0f
github username <github email address>
2017-03-28 12:09:40 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,36 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride 
+      if user.tickets < attraction.tickets && user.height < attraction.min_height
+        return "Sorry. " + tickets_height
+      elsif user.tickets < attraction.tickets
+        return "Sorry. " + tickets
+      elsif user.height < attraction.min_height
+        return "Sorry. " + height
+      end
+
+      user.tickets = user.tickets - attraction.tickets
+      user.nausea = user.nausea + attraction.nausea_rating
+      user.happiness = user.happiness + attraction.happiness_rating
+      user.save
+  end
+
+  def thanks
+    "Thanks for riding the #{attraction.name}!"
+  end 
+  
+  def tickets_height
+    "You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+  end
+
+  def tickets
+    "You do not have enough tickets to ride the #{attraction.name}."
+  end 
+
+  def height
+    "You are not tall enough to ride the #{attraction.name}."
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
-  # write associations here
+
+  has_many :rides
+  has_many :attractions, through: :rides
+  
+  has_secure_password
+
+  def mood 
+    if self.happiness > self.nausea 
+      return "happy"
+    end
+    if self.happiness < self.nausea 
+      return "sad"
+    end
+  end
+  
 end
